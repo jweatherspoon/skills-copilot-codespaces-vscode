a63e8e676d7b35627a4633003224485470b47c2e
Jon Weatherspoon <jweatherspoon.se@gmail.com>
2025-01-03 20:02:00 +0000

    third

---
+++ comments.js
@@ -0,0 +1,73 @@
+// create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// store comments
+const commentsByPostId = {};
+
+// get comments
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+// post comments
+app.post('/posts/:id/comments', async (req, res) => {
+  const { content } = req.body;
+  const commentId = Math.random().toString(36).substring(2);
+  const postId = req.params.id;
+  const comments = commentsByPostId[postId] || [];
+
+  comments.push({ id: commentId, content, status: 'pending' });
+  commentsByPostId[postId] = comments;
+
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId,
+      status: 'pending'
+    }
+  });
+
+  res.status(201).send(comments);
+});
+
+// post event
+app.post('/events', async (req, res) => {
+  console.log('Received Event:', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { id, postId, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        status,
+        content
+      }
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
